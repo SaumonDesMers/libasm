a76617dd03f7a00016cb08d513e31c88cfa27a1c
¨Simon¨ <sim.gaubert.sg@gmail.com>
2025-03-25 18:26:01 +0100

    ADD ft_strcpy

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 int ft_strlen(char *);
+char *ft_strcpy(char * dest, const char * src);
 
 int main()
 {
 	char * str = "Hello world!";
 	printf("%i\n", ft_strlen(str));
+
+	char dest[13];
+	ft_strcpy(dest, str);
+	printf("%s\n", dest);
+
 	return 0;
 }
---
+++ src/strcpy.s
@@ -0,0 +1,14 @@
+section .text
+	global ft_strcpy
+
+ft_strcpy:
+	mov rax, rdi		; save str in rax
+
+loop_start:
+	cmp BYTE [rsi], 0	; test end of string
+	je loop_end			; while (*src != 0) {
+	movsb				; 	*dest++ = *src++
+	jmp loop_start		; }
+loop_end:
+
+	ret
