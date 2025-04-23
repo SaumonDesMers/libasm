e3c1770b7d6b14d3b83ac78dccd7c919da199e2e
Simon <sim.gaubert.sg@gmail.com>
2025-04-23 12:34:00 +0200

    ADD write comment

--- src/write.s
+++ src/write.s
@@ -4,16 +4,16 @@ section .text
 	global ft_write
 
 ft_write:
-	mov rax, 1
-	syscall
-	cmp rax, 0
-	jl error
+	mov rax, 1						; syscall number for write
+	syscall							; call the kernel
+	cmp rax, 0						; check if the return value is negative
+	jl error 						; if it is, jump to error handling
 	ret
 
 error:
-	neg rax
-	mov rdi, rax
-	call __errno_location WRT ..plt
-	mov [rax], rdi
-	mov rax, -1
+	neg rax							; negate rax to get the error code
+	mov rdi, rax					; move the error code to rdi to save it
+	call __errno_location WRT ..plt	; call __errno_location to get the address of errno
+	mov [rax], rdi					; store the error code in errno
+	mov rax, -1						; set rax to -1 to indicate an error
 	ret
