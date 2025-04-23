691e4d5bfa8f7b1e6ad11f8acda9c94a84b0c0c9
Simon <sim.gaubert.sg@gmail.com>
2025-04-23 12:34:09 +0200

    ADD ft_read

---
+++ src/read.s
@@ -0,0 +1,19 @@
+extern __errno_location
+
+section .text
+	global ft_read
+
+ft_read:
+	mov rax, 0						; syscall number for read
+	syscall							; call the kernel
+	cmp rax, 0						; check if the return value is negative
+	jl error 						; if it is, jump to error handling
+	ret
+
+error:
+	neg rax							; negate rax to get the error code
+	mov rdi, rax					; move the error code to rdi to save it
+	call __errno_location WRT ..plt	; call __errno_location to get the address of errno
+	mov [rax], rdi					; store the error code in errno
+	mov rax, -1						; set rax to -1 to indicate an error
+	ret
