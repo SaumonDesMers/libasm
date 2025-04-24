90f264c28b31f2a43420657db8b8c3f20662bdf0
Simon <sim.gaubert.sg@gmail.com>
2025-04-24 16:28:35 +0200

    UPDATE clean atoi_base comment

--- src/atoi_base.s
+++ src/atoi_base.s
@@ -6,14 +6,12 @@ ft_atoi_base:
 	xor rax, rax			; init return value
 	mov rdx, 1				; use rdx to store the sign
 
-
 	; setup hashtable in the stack
 	mov r9, rsp				; use rsp for hashtable start
 	mov rcx, 16
 hashtable_loop:
 	push QWORD 0			; push 8 bytes 16 times to get 128 bytes
 	loop hashtable_loop
-
 
 	; check if arguments are non-NULL
 	cmp rdi, 0
@@ -23,7 +21,7 @@ hashtable_loop:
 
 	; check if base is valid, get it's lenght and setup the hashtable
 	mov r8, rsi				; use r8 to iterate over the base
-	mov cl, 0				; use cl as the base lenght counter
+	xor cl, cl				; use cl as the base lenght counter
 base_loop_start:
 	cmp BYTE [r8], 0		; check end of str
 	je base_loop_end
@@ -35,15 +33,15 @@ base_loop_start:
 
 	mov r10, r9				; get the hashtable start
 	mov r12, 0
-	mov r12b, BYTE [r8]
-	sub r10, r12		; sub the character value to get it's emplacement (hash function)
+	mov r12b, BYTE [r8]		; isolate the character value to substract it easily
+	sub r10, r12			; sub the character value to get it's emplacement (hash function)
 	cmp BYTE [r10], 0		; check if stored value is zero
 	jne end					; if it's not zero, that means there is a duplicated character
 
 	mov BYTE [r10], cl		; store the character emplacement in the base string
-	inc BYTE [r10]			; add one to get the one-based indexing
+	inc BYTE [r10]			; add one to get the one-based indexing to detect the first character
 
-	inc cl
+	inc cl					; increment the base size
 	inc r8					; increment str ptr
 	jmp base_loop_start
 base_loop_end:
@@ -53,7 +51,7 @@ base_loop_end:
 
 
 	; ignore leading whitespace
-	dec rdi
+	dec rdi					; decrement first so that we can increment at the start of the loop
 whitespace_loop_start:
 	inc rdi
 	cmp BYTE [rdi], 0		; check end of str
@@ -83,10 +81,10 @@ sign_loop_start:
 	cmp BYTE [rdi], '-'		; check if there is a '-'
 	je minus
 
-	jmp sign_loop_end
+	jmp sign_loop_end		; break if there is no '+' or '-'
 
-minus:						; if there is a '-', inverse the sign
-	neg rdx					; negate rdx to reflect number sign
+minus:						; if there is a '-'
+	neg rdx					; negate rdx to inverse the sign
 plus:						; if there is a '+' just pass
 	inc rdi					; increment str ptr
 	jmp sign_loop_start
@@ -94,7 +92,7 @@ plus:						; if there is a '+' just pass
 sign_loop_end:
 
 
-	; finaly translate the number
+	; finaly convert the number
 number_loop_start:
 	cmp BYTE [rdi], 0		; check end of str
 	je end
@@ -103,7 +101,7 @@ number_loop_start:
 
 	mov r10, r9				; get the hashtable start
 	mov r12, 0
-	mov r12b, BYTE [rdi]
+	mov r12b, BYTE [rdi]	; isolate the character value to substract it easily
 	sub r10, r12			; sub the character value to get it's emplacement (hash function)
 	cmp BYTE [r10], 0		; check if stored value is zero
 	je end					; if it's zero, that means this character is not part of the base so it's the end
@@ -111,7 +109,7 @@ number_loop_start:
 	add al, BYTE [r10]		; add the next number to the result
 	dec rax
 
-	inc rdi
+	inc rdi					; increment str ptr
 	jmp number_loop_start
 
 end:
