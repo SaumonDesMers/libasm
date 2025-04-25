7ee08a8b9c35d3e096edebb1e168923d9d3e31b9
Simon <sim.gaubert.sg@gmail.com>
2025-04-25 17:09:42 +0200

    FIX ft_strcpy

--- src/strcpy.s
+++ src/strcpy.s
@@ -2,13 +2,14 @@ section .text
 	global ft_strcpy
 
 ft_strcpy:
-	mov rax, rdi		;save str in rax
+	mov rax, rdi		; save str in rax
 
 loop_start:
-	cmp BYTE [rsi], 0	;test end of string
-	je loop_end			;while (*src != 0) {
-	movsb				;	*dest++ = *src++
-	jmp loop_start		;}
+	cmp BYTE [rsi], 0	; test end of string
+	je loop_end			; while (*src != 0) {
+	movsb				; 	*dest++ = *src++
+	jmp loop_start		; }
 loop_end:
 
+	mov BYTE [rdi], 0	; add null terminator
 	ret
